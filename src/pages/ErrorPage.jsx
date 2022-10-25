424b9ccc508a5b7f3a1a87cd398c3198003bd78f
Naimur <naeemhasan28@gmail.com>
2022-10-26 00:02:44 +0600

    added error page button

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,4 +1,5 @@
-import { useRouteError } from 'react-router-dom'
+import { Link, useRouteError } from 'react-router-dom'
+import { motion } from 'framer-motion'
 
 export default function ErrorPage() {
   const error = useRouteError()
@@ -11,6 +12,22 @@ export default function ErrorPage() {
       <p className='text-xl'>
         <i>{error.statusText || error.message}</i>
       </p>
+
+      <motion.button
+        className='bg-green-600 pt-1 pb-2 px-6 rounded-md mt-8 transition-all shadow-md'
+        whileHover={{
+          scale: [1, 1.1, 1, 1.1, 1],
+          transition: {
+            duration: 0.1,
+            repeat: 3,
+            repeatType: 'loop',
+          },
+        }}>
+        <Link to='/'>
+          <span className='text-xl'>&larr;</span>
+          <span className='pl-3'>Go Home</span>
+        </Link>
+      </motion.button>
     </div>
   )
 }
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Homepage = () => {
   return (
-    <div className='flex items-cente h-layout'>
-      <h1>This is homepage</h1>
+    <div className='flex items-center justify-center h-layout'>
+      <div className='grid items-center text-center homepage-card max-w-[800px] rounded-lg p-10 shadow-xl'>
+        <h3 className='text-5xl font-hand'>Welcome to Framer motion walktrough</h3>
+        <h4 className='text-xl pt-4 pb-5 italic'>
+          You can use it as a framer motion cheat sheet or guide (The Codebase)
+        </h4>
+        <p className='text-6xl'>Peace ✌️</p>
+      </div>
     </div>
   )
 }
