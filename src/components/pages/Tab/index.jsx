d54ba1aaff27d1f109c7cf9ce7ac32d334f600db
Naimur Rahman <64221853+mohammad-naimur-rahman@users.noreply.github.com>
2023-01-05 22:12:58 +0600

    added tab component in layout page

--- src/components/pages/Tab/index.jsx
+++ src/components/pages/Tab/index.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import { useState } from 'react'
+import { initialTabs as tabs } from './ingredients'
+import { motion, AnimatePresence } from 'framer-motion'
 
-const Tab = () => {
+export default function Tab() {
+  const [selectedTab, setSelectedTab] = useState(tabs[0])
+
   return (
-    <div>
-      <p>Hello</p>
+    <div className='tab'>
+      <div className='window'>
+        <nav>
+          <ul>
+            {tabs.map(item => (
+              <li
+                key={item.label}
+                className={item === selectedTab ? 'selected' : ''}
+                onClick={() => setSelectedTab(item)}>
+                {`${item.icon} ${item.label}`}
+                {item === selectedTab ? <motion.div className='underline' layoutId='underline' /> : null}
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <main>
+          <AnimatePresence exitBeforeEnter>
+            <motion.div
+              key={selectedTab ? selectedTab.label : 'empty'}
+              initial={{ y: 10, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              exit={{ y: -10, opacity: 0 }}
+              transition={{ duration: 0.2 }}>
+              {selectedTab ? selectedTab.icon : '😋'}
+            </motion.div>
+          </AnimatePresence>
+        </main>
+      </div>
     </div>
   )
 }
-
-export default Tab
